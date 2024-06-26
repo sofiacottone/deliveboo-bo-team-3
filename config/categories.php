a8be80640111e904d496752a4a50d2c97a1d2c8d
sofiacottone <cottonesofia@outlook.it>
2024-06-26 10:14:31 +0200

    feat: add categories db

---
+++ config/categories.php
@@ -0,0 +1,161 @@
+<?php
+
+return [
+    [
+        "name" => "italiano",
+        "image" => "",
+    ],
+    [
+        "name" => "internazionale",
+        "image" => "",
+    ],
+    [
+        "name" => "cinese",
+        "image" => "",
+    ],
+    [
+        "name" => "giapponese",
+        "image" => "",
+    ],
+    [
+        "name" => "messicano",
+        "image" => "",
+    ],
+    [
+        "name" => "indiano",
+        "image" => "",
+    ],
+    [
+        "name" => "pesce",
+        "image" => "",
+    ],
+    [
+        "name" => "carne",
+        "image" => "",
+    ],
+    [
+        "name" => "pizza",
+        "image" => "",
+    ],
+    [
+        'name' => 'messicano',
+        'image' => ''
+    ],
+    [
+        'name' => 'vegetariano',
+        'image' => ''
+    ],
+    [
+        'name' => 'vegano',
+        'image' => ''
+    ],
+    [
+        'name' => 'fast food',
+        'image' => ''
+    ],
+    [
+        'name' => 'barbecue',
+        'image' => ''
+    ],
+    [
+        'name' => 'frutti di mare',
+        'image' => ''
+    ],
+    [
+        'name' => 'mediterraneo',
+        'image' => ''
+    ],
+    [
+        'name' => 'francese',
+        'image' => ''
+    ],
+    [
+        'name' => 'spagnolo',
+        'image' => ''
+    ],
+    [
+        'name' => 'thailandese',
+        'image' => ''
+    ],
+    [
+        'name' => 'coreano',
+        'image' => ''
+    ],
+    [
+        'name' => 'vietnamita',
+        'image' => ''
+    ],
+    [
+        'name' => 'gourmet',
+        'image' => ''
+    ],
+    [
+        'name' => 'fusion',
+        'image' => ''
+    ],
+    [
+        'name' => 'steakhouse',
+        'image' => ''
+    ],
+    [
+        'name' => 'libanese',
+        'image' => ''
+    ],
+    [
+        'name' => 'greco',
+        'image' => ''
+    ],
+    [
+        'name' => 'turco',
+        'image' => ''
+    ],
+    [
+        'name' => 'arabo',
+        'image' => ''
+    ],
+    [
+        'name' => 'africano',
+        'image' => ''
+    ],
+    [
+        'name' => 'caraibico',
+        'image' => ''
+    ],
+    [
+        'name' => 'brasiliano',
+        'image' => ''
+    ],
+    [
+        'name' => 'peruviano',
+        'image' => ''
+    ],
+    [
+        'name' => 'tailandese',
+        'image' => ''
+    ],
+    [
+        'name' => 'birreria',
+        'image' => ''
+    ],
+    [
+        'name' => 'pasticceria',
+        'image' => ''
+    ],
+    [
+        'name' => 'gelateria',
+        'image' => ''
+    ],
+    [
+        'name' => 'paninoteca',
+        'image' => ''
+    ],
+    [
+        'name' => 'taverna',
+        'image' => ''
+    ],
+    [
+        'name' => 'trattoria',
+        'image' => ''
+    ],
+
+];
