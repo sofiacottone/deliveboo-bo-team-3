414d61f284ede153e3f8c0d45922631523ae1145
sofiacottone <cottonesofia@outlook.it>
2024-06-26 10:46:16 +0200

    feat: add DashboardController

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+        $data = [
+            'user' => $user,
+        ];
+        return view('admin.dashboard', $data);
+    }
+}
