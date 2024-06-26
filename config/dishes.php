45faf78bd7210fffd4356c05608b8f2f3110d80e
sofiacottone <cottonesofia@outlook.it>
2024-06-26 10:15:08 +0200

    feat: add dishes db

---
+++ config/dishes.php
@@ -0,0 +1,180 @@
+<?php
+
+return [
+    [
+        'name' => 'Riso alla Cantonese',
+        'description' => 'Riso fritto con piselli, carote, prosciutto e uova.',
+        'price' => 8.50,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Pizza Margherita',
+        'description' => 'Pizza classica con pomodoro, mozzarella e basilico.',
+        'price' => 6.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Spaghetti alle Vongole',
+        'description' => 'Spaghetti con vongole, aglio, olio e prezzemolo.',
+        'price' => 12.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Lasagne alla Bolognese',
+        'description' => 'Lasagne al forno con ragù di carne e besciamella.',
+        'price' => 10.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Tacos al Pastor',
+        'description' => 'Tacos di maiale marinato con ananas, cipolla e coriandolo.',
+        'price' => 9.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Sushi Misto',
+        'description' => 'Assortimento di sushi con nigiri, sashimi e maki.',
+        'price' => 15.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Pollo al Curry',
+        'description' => 'Pollo cotto in una salsa di curry con riso basmati.',
+        'price' => 11.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Insalata di Quinoa',
+        'description' => 'Insalata fresca con quinoa, verdure e vinaigrette.',
+        'price' => 7.50,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Burger Vegano',
+        'description' => 'Burger vegano con patty di legumi e verdure fresche.',
+        'price' => 8.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Cheeseburger',
+        'description' => 'Burger classico con formaggio cheddar e patatine.',
+        'price' => 7.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Costine BBQ',
+        'description' => 'Costine di maiale cotte alla griglia con salsa barbecue.',
+        'price' => 14.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Gamberi alla Griglia',
+        'description' => 'Gamberi freschi grigliati con limone e aglio.',
+        'price' => 13.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Paella Valenciana',
+        'description' => 'Paella con pollo, coniglio, fagioli e verdure.',
+        'price' => 16.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Croque Monsieur',
+        'description' => 'Sandwich francese con prosciutto, formaggio e besciamella.',
+        'price' => 6.50,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Tapas Miste',
+        'description' => 'Assortimento di tapas spagnole con vari ingredienti.',
+        'price' => 12.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Pad Thai',
+        'description' => 'Noodles tailandesi saltati in padella con gamberi, uova e arachidi.',
+        'price' => 9.50,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Bibimbap',
+        'description' => 'Ciotola coreana di riso con carne, verdure e uovo fritto.',
+        'price' => 11.50,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Pho',
+        'description' => 'Zuppa vietnamita di noodles con carne e verdure.',
+        'price' => 8.00,
+        'visibility' => true,
+        'image' => 'https://www.leitv.it/wp-content/uploads/2016/07/pho-ricetta.jpg?v=55723'
+    ],
+    [
+        'name' => 'Filetto Gourmet',
+        'description' => 'Filetto di manzo con contorno di verdure e salsa al vino rosso.',
+        'price' => 20.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Poke Bowl',
+        'description' => 'Ciotola hawaiana con pesce crudo, riso e verdure.',
+        'price' => 10.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Bistecca alla Fiorentina',
+        'description' => 'Bistecca di manzo alla griglia servita con fagioli e spinaci.',
+        'price' => 25.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Falafel',
+        'description' => 'Polpette di ceci fritte servite con salsa tahina.',
+        'price' => 6.50,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Moussaka',
+        'description' => 'Piatto greco con melanzane, carne macinata e besciamella.',
+        'price' => 10.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Kebab',
+        'description' => 'Carne arrostita servita con pane pita e verdure.',
+        'price' => 7.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+    [
+        'name' => 'Tajine di Agnello',
+        'description' => 'Stufato marocchino di agnello con prugne e mandorle.',
+        'price' => 12.00,
+        'visibility' => true,
+        'image' => ''
+    ],
+
+];
