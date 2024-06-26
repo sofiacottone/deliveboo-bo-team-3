8d758204d7bfbf63b8f1f9d43f58a947a8c88691
sofiacottone <cottonesofia@outlook.it>
2024-06-26 10:15:35 +0200

    feat: add migrations

---
+++ database/migrations/2024_06_25_122843_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price', 7, 2);
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('status');
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
