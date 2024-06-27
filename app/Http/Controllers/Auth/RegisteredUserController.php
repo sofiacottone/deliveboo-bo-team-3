29d1c4e787494c44577b8ce2d67f604a7372f3d2
sofiacottone <cottonesofia@outlook.it>
2024-06-27 15:12:00 +0200

    feat: add validation + add new istance of restaurant

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Restaurant;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -12,6 +14,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -20,7 +23,8 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $categories = Category::all();
+        return view('auth.register', compact('categories'));
     }
 
     /**
@@ -30,17 +34,38 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // validate data 
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'address' => 'required|min:5|max:250',
+            'VAT_no' => 'required|min:11|max:13|unique:restaurants',
+            'restaurant_name' => 'required|min:2',
+            'description' => 'nullable|min:5',
+            'image' => 'nullable|image|max:512',
+            'categories' => 'required|exists:categories,id'
         ]);
 
+        // create new user 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $formData = $request->all();
+
+        // create new restaurant 
+        $newRestaurant = new Restaurant();
+        $newRestaurant->fill($formData);
+        $newRestaurant->slug = Str::slug($newRestaurant->restaurant_name, '-');
+        $newRestaurant->save();
+
+        // add relationship
+        if ($request->has('categories')) {
+            $newRestaurant->categories()->attach($formData['categories']);
+        }
 
         event(new Registered($user));
 
