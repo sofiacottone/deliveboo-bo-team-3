54b92c429508d3deb0dcb9daccb0aa411664dda3
sofiacottone <cottonesofia@outlook.it>
2024-06-27 10:23:58 +0200

    feat: change home route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/admin/dashboard';
+    // public const HOME = '/admin/dashboard';
+    public const HOME = '/admin/register-restaurant';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
