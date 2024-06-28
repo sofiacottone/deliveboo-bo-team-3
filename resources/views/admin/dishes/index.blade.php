06e7c17105e9f776ac73768f934e16bcfe10f7a1
sofiacottone <cottonesofia@outlook.it>
2024-06-28 17:27:36 +0200

    feat: add delete btn

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,35 +1,49 @@
 @extends('layouts.admin')
 
 @section('content')
-<H3 class="text-center">I tuoi Piatti</H3>
-<div class="d-flex">
-    <h4>Aggiungi un piatto:</h4>
-    <a class="text-body" href="{{Route('admin.menu.create')}}"><i class="fa-solid fa-plus"></i></a>
-</div>
-<table class="table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">NOME</th>
-        <th scope="col">SLUG</th>
-        <th scope="col">PREZZO</th>
-        <th scope="col">DESCRIZIONE</th>
-        <th scope="col">AZIONI</th>
-      </tr>
-    </thead>
-    <tbody>
-    @foreach ($dishes as $dish)
-        <tr>
-            <td>{{$dish->name}}</td>
-            <td>{{$dish->slug}}</td>
-            <td>{{$dish->price}}</td>
-            <td>{{$dish->description}}</td>
-            <td>
-                <a class="text-body" href="{{ route('admin.menu.show', $dish->id) }}"><i class="fa-solid fa-info"></i></a>
-                <i class="fa-solid fa-pen-to-square"></i>
-                <i class="fa-solid fa-trash"></i>
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+    <H3 class="text-center">I tuoi Piatti</H3>
+    <div class="d-flex">
+        <h4>Aggiungi un piatto:</h4>
+        <a class="text-body" href="{{ Route('admin.menu.create') }}"><i class="fa-solid fa-plus"></i></a>
+    </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">NOME</th>
+                <th scope="col">SLUG</th>
+                <th scope="col">PREZZO</th>
+                <th scope="col">DESCRIZIONE</th>
+                <th scope="col" class="text-center">AZIONI</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($dishes as $dish)
+                <tr>
+                    <td>{{ $dish->name }}</td>
+                    <td>{{ $dish->slug }}</td>
+                    <td>{{ $dish->price }}</td>
+                    <td>{{ $dish->description }}</td>
+                    {{-- actions --}}
+                    <td class="d-flex justify-content-center align-items-center gap-2">
+                        <a class="text-body btn btn-outline-secondary" href="{{ route('admin.menu.show', $dish->id) }}">
+                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                        </a>
+
+                        <a href="" class="btn btn-outline-secondary">
+                            <i class="fa-solid fa-pencil"></i>
+                        </a>
+
+                        <form action="{{ route('admin.menu.destroy', ['menu' => $dish->id]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" data-dish-name="{{ $dish->name }}"
+                                class="js-delete-btn btn btn-outline-danger">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
