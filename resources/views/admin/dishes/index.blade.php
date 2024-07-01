bd60f246d144937dbf1ffb5820c37833ee51827d
Alfredodaniello <alfredo_daniello@libero.it>
2024-07-01 17:14:08 +0200

    feat: add cards on index

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -53,6 +53,46 @@
         </tbody>
     </table>
 
+    <div class="container">
+        <div class="row row-cols-4 gx-4 gy-4">
+            @foreach ($dishes as $dish)
+                <div class="col">
+                    <div class="card h-100 mb-3 {{ $dish->visibility == 0 ? 'disabled' : '' }}">
+                        <div class="card-body">
+                            <img class="card-img-top mb-1" src="{{ asset('storage/'. $dish->image) }}" alt="Card image cap">
+                            <h5 class="card-title">{{ $dish->name }}</h5>
+                            <h6 class="card-title"><i class="fa-solid fa-euro-sign mb-1"></i> {{ $dish->price }}</h6>
+                            <h6 class="card-title">Visibile nel menù:
+                                @if ($dish->visibility == 1)
+                                    <i class="fa-solid fa-eye"></i>
+                                @else
+                                    <i class="fa-solid fa-eye-slash"></i>
+                                @endif
+                            </h6>
+                            <hr class="mb-3">
+                            <div class="d-flex justify-content-center">
+                                <a class="text-body btn btn-outline-secondary" href="{{ route('admin.menu.show', $dish->slug) }}">
+                                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                                </a>
+                                <a href="{{ route('admin.menu.edit', $dish->slug) }}" class="btn btn-outline-secondary mx-2">
+                                    <i class="fa-solid fa-pencil"></i>
+                                </a>
+                                <form action="{{ route('admin.menu.destroy', ['dish' => $dish->id]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" data-dish-name="{{ $dish->name }}"
+                                        class="js-delete-btn btn btn-outline-danger">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
     {{-- modal  --}}
     <div id="confirmDeleteModal" class="ms-modal">
 
