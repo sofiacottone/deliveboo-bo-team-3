e1e2c125c0f71afdebb58821c07cf8e18820f2a6
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-07-01 14:43:24 +0200

    feat: add custom logo and translation of page in Auth folder

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -34,7 +34,7 @@
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Invia il link per resettare la password') }}
                                 </button>
                             </div>
                         </div>
