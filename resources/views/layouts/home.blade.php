3cb3e5e94d0e9aa0450fc6e9788966ca9bc813da
sofiacottone <cottonesofia@outlook.it>
2024-07-01 17:17:23 +0200

    feat: add home layout

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-center">benvenuto nel back-office</div>
+    <h3 class="text-center mt-4">Sei un ristoratore?</h3>
+    <div class="container my-4">
+        <div class="d-flex justify-content-center gap-3">
+            <div class="card h-100 w-100">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Login</h5>
+                    <div class="card-text my-3 text-center">Clicca qui per effettuare il login.</div>
+                    @guest
+                        <div class="d-flex justify-content-center">
+                            <div class="ms-btn-custom">
+                                <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </div>
+                        </div>
+                    @endguest
+                </div>
+            </div>
+
+            <div class="card w-100">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Registrati</h5>
+                    <div class="card-text my-3 text-center">Clicca qui per registrarti.</div>
+                    @guest
+                        <div class="d-flex justify-content-center">
+                            @if (Route::has('register'))
+                                <div class="ms-btn-custom">
+                                    <a href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                                </div>
+                            @endif
+                        </div>
+                    @endguest
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
