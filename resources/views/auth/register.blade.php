482c35329c44b37468bee5f3be8209ef765f7116
sofiacottone <cottonesofia@outlook.it>
2024-07-01 17:16:51 +0200

    feat: add custom btn

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -166,7 +166,7 @@
 
                             <div class="mb-4 row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="ms-btn-custom">
                                         {{ __('Registrati') }}
                                     </button>
                                 </div>
