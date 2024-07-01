68bef5a2eedd2a077e41ada0eab273dfaad2c013
sofiacottone <cottonesofia@outlook.it>
2024-07-01 10:49:22 +0200

    feat: add force delete btn

--- resources/views/admin/dishes/deleted.blade.php
+++ resources/views/admin/dishes/deleted.blade.php
@@ -27,14 +27,14 @@
                                 <i class="fa-solid fa-trash-can-arrow-up"></i>
                             </a>
 
-                            {{-- <form action="{{ route('admin.menu.destroy', ['menu' => $dish->id]) }}" method="POST">
+                            <form action="{{ route('admin.menu.forceDelete', ['menu' => $dish->id]) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" data-dish-name="{{ $dish->name }}"
                                     class="js-delete-btn btn btn-outline-danger">
                                     <i class="fa-solid fa-trash"></i>
                                 </button>
-                            </form> --}}
+                            </form>
                         </td>
                     </tr>
                 @endforeach
@@ -52,14 +52,14 @@
             <div class="hstack align-items-center justify-content-between mb-3">
                 <div class="hstack align-items-center justify-content-center gap-2">
                     <img src="https://img.icons8.com/ios/40/000000/box-important--v1.png" alt="box-important--v1" />
-                    <div class="fs-3">Confirm delete</div>
+                    <div class="fs-3">Conferma cancellazione definitiva</div>
                 </div>
                 <div class="ms-close">&times;</div>
             </div>
             <div class="ms-modal-body"></div>
-            <div class="hstack justify-content-end gap-2">
-                <button class="ms-close-btn btn btn-secondary">Cancel</button>
-                <button id="modal-confirm" class="btn btn-danger">Delete</button>
+            <div class="hstack justify-content-end gap-2 mt-2">
+                <button class="ms-close-btn btn btn-secondary">Annulla</button>
+                <button id="modal-confirm" class="btn btn-danger">Elimina</button>
             </div>
         </div>
 
