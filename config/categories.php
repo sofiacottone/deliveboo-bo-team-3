5a3ded70750536d3d24b0e5ab0faf545b5cda4f8
DanieleBegnis <daniele.begnis@gmail.com>
2024-07-03 16:30:45 +0200

    removed categories image, slimmed down categories

--- config/categories.php
+++ config/categories.php
@@ -3,47 +3,36 @@
 return [
     [
         "name" => "italiano",
-        "image" => 'resources/img/flag-italy_1f1ee-1f1f9.png',
     ],
     [
         "name" => "cinese",
-        "image" => 'resources\img\flag-taiwan_1f1f9-1f1fc.png',
     ],
     [
         "name" => "giapponese",
-        "image" => 'resources\img\flag-japan_1f1ef-1f1f5.png',
     ],
     [
         "name" => "messicano",
-        "image" => 'resources\img\flag-mexico_1f1f2-1f1fd.png',
     ],
     [
         "name" => "indiano",
-        "image" => 'resources\img\flag-india_1f1ee-1f1f3.png',
     ],
     [
         "name" => "pizza",
-        "image" => "resources\img\pizza.png",
     ],
     [
         'name' => 'greco',
-        'image' => 'resources\img\flag-greece_1f1ec-1f1f7.png'
     ],
     [
         'name' => 'francese',
-        'image' => 'resources\img\flag-france_1f1eb-1f1f7.png'
     ],
     [
         'name' => 'thailandese',
-        'image' => 'resources\img\flag-thailand_1f1f9-1f1ed.png'
     ],
     [
         'name' => 'vietnamita',
-        'image' => 'resources\img\flag-vietnam_1f1fb-1f1f3.png'
     ],
     [
         'name' => 'coreano',
-        'image' => 'resources\img\flag-south-korea_1f1f0-1f1f7.png'
     ],
 
 ];
