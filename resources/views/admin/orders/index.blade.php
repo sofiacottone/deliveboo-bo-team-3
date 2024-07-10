8233e837a2fb7cbfb070aeefa1b5d5502efe9d6a
DanieleBegnis <daniele.begnis@gmail.com>
2024-07-10 17:12:34 +0200

    fix order table responsive

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -7,24 +7,24 @@
     <table class="table table-striped">
         <thead>
             <tr>
-                <th scope="col" class="text-uppercase">nome</th>
+                <th scope="col" class="text-uppercase ms-hide-table">nome</th>
                 <th scope="col" class="text-uppercase">prezzo</th>
                 <th scope="col" class="text-uppercase">indirizzo</th>
                 <th scope="col" class="text-uppercase">telefono</th>
-                <th scope="col" class="text-uppercase">email</th>
-                <th scope="col" class="text-uppercase">data</th>
+                <th scope="col" class="text-uppercase ms-hide-table">email</th>
+                <th scope="col" class="text-uppercase ms-hide-table">data</th>
                 <th scope="col" class="text-center text-uppercase">azioni</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($orders as $order)
                 <tr>
-                    <td>{{ $order->full_name }}</td>
+                    <td class="ms-hide-table">{{ $order->full_name }}</td>
                     <td>{{ $order->price }}€</td>
                     <td>{{ $order->address }}</td>
                     <td>{{ $order->phone_number }}</td>
-                    <td>{{ $order->email }}</td>
-                    <td>{{ $order->created_at->format('d/m/Y h:i:s a') }}</td>
+                    <td class="ms-hide-table">{{ $order->email }}</td>
+                    <td class="ms-hide-table">{{ $order->created_at->format('d/m/Y h:i:s a') }}</td>
 
                     {{-- actions --}}
                     <td class="d-flex justify-content-center align-items-center gap-2">
@@ -40,4 +40,11 @@
             @endforeach
         </tbody>
     </table>
+    <style>
+        @media screen and (max-width: 768px) {
+            .ms-hide-table {
+                display: none;
+            }
+        }
+    </style>
 @endsection
