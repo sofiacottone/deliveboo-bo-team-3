038bcb142049bb2002415316c36c1d132851d5f6
sofiacottone <cottonesofia@outlook.it>
2024-07-10 22:25:10 +0200

    fix: main height

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -53,8 +53,7 @@
 
         <div class="container-fluid vh-100">
             <div class="row h-100">
-                <nav id="sidebarMenu"
-                    class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column gap-1 pb-2">
                             <li class="nav-item">
@@ -97,7 +96,7 @@
                     </div>
                 </nav>
 
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-3">
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-3 overflow-y-auto h-100">
                     @yield('content')
                 </main>
             </div>
